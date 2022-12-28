1ec0acd57ce9562cf14ebaee73c707ea7f8e7121
Your Name <you@example.com>
2022-12-27 21:48:13 -0500

    initial project

---
+++ src/main/java/org/example/Concatenation.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Concatenation {
+    public static void main (String[] args){
+        String nombre = "Eduardo";
+        Integer followersInstagram = 200;
+        Integer followersYoutube = 200;
+        String frase = "Hola, mi nombre es " + nombre + ". Tengo " + (followersYoutube + followersInstagram) + " seguidores";
+        System.out.println(frase);
+    }
+}
