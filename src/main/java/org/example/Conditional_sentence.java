81a0bbf958bae8929feae8268b37797682c46d24
Your Name <you@example.com>
2022-12-27 22:03:35 -0500

    conditional sentence

---
+++ src/main/java/org/example/Conditional_sentence.java
@@ -0,0 +1,12 @@
+package org.example;public class Conditional_sentence {
+    public static void main(String[] args){
+        Integer edad = 18;
+        if (edad > 1){
+            if (edad >= 18) {
+                System.out.println("Es mayor de edad");
+            } else {
+                System.out.println("Es menor de edad");
+            }
+        }
+    }
+}
